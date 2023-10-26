d591c89304eed0412e0095c46313c43208d2718b
Rita Martins <mazedoodle@gmail.com>
2023-10-26 23:24:15 +0100

    finished logic

--- src/stack/stack-dom.js
+++ src/stack/stack-dom.js
@@ -2,28 +2,40 @@ const stackList = document.getElementById('stack-list');
 const stackInput = document.getElementById('stack-input');
 const container = document.getElementById('container');
 const warningTopStack = document.querySelector('#stack-container .warning-top');
-const warningBottomStack = document.querySelector('#stack-container .warning-bottom');
+const warningBottomStack = document.querySelector(
+  '#stack-container .warning-bottom'
+);
 const addStackBtn = document.getElementById('add-stack');
 const takeStackBtn = document.getElementById('take-stack');
 
+// creates a new stack object
 const newStack = new Stack();
 
+// function that deletes the text on the input box
 const clearStackInput = () => {
   stackInput.value = '';
 };
 
+// makes the task list visible. runs the first time, is triggered every time we click the button
 const renderListStack = () => {
+  // hides top warning
   warningTopStack.style.display = 'none';
+
+  // hides bottom warning
   warningBottomStack.style.display = 'none';
+
+  // assumption: replaces all the content every time it is called, rendering the correct amount of filled and empty cells. Without it, a new stack is added, and not created
   stackList.innerHTML = '';
   let length = newStack.display().length;
   let size = newStack.MAX_SIZE - length;
+  // this is creating the blue items
   newStack.display().forEach(item => {
     let li = document.createElement('li');
     li.className = 'active';
     li.innerText = item;
     stackList.appendChild(li);
   });
+  // this is creating the gray squares. grey squares are the # of elements not occupied fot the array, hence a for loop that can stop with size
   for (let i = 0; i < size; i++) {
     let li = document.createElement('li');
     li.className = 'inactive';
@@ -33,6 +45,7 @@ const renderListStack = () => {
 };
 renderListStack();
 
+// function takes a type parameter. if a certain type, will display the error
 const generateWarningStack = type => {
   if (type === 'underflow') {
     warningBottomStack.style.display = 'block';
@@ -43,6 +56,7 @@ const generateWarningStack = type => {
   }
 };
 
+// adds a new value to the stack
 const addToStack = () => {
   if (newStack.push(stackInput.value) === 'Stack Overflow') {
     generateWarningStack('overflow');
